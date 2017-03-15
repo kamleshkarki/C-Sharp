d2a2dd91da8b37d234ab2f05dce40ab8197fdb52
Kamlesh Singh karki <kkarki00@gmail.com>
2017-03-16 00:06:05 +0530

    check if else
    Loop

---
+++ Basic_program/run.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePrograms
+{
+    class run
+    {
+        public void go()
+        {
+
+            Console.WriteLine("Enter the number :");
+            Decimal var1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter the Second number :");
+            Decimal var2 = Convert.ToDecimal(Console.ReadLine());
+            if (var1 > var2)
+            {
+                Console.WriteLine("First is greater");
+            }
+            else if (var1 == var2)
+            {
+                Console.WriteLine("Number are the same");
+            }
+            else
+            {
+                Console.WriteLine("Second is greater");
+            }
+            Console.ReadLine();
+
+        }
+        static void Main(string[] args)
+        {
+            run s2 = new run();
+            s2.go();
+            Console.ReadLine();
+        }
+    }
+}
+
+
