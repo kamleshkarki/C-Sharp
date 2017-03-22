bdde11315e9a11832680115a01f6730f31eafcae
Kamlesh Singh karki <kkarki00@gmail.com>
2017-03-22 19:36:56 +0530

    Parameterized Constructor
    This is the Example of Parameterized Constructor

--- constructor/Program.cs
+++ constructor/Program.cs
@@ -1,29 +1,27 @@
-﻿/*This is the Example of default constructor*/
+﻿/*This is the Example of Parameterized Constructor */
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleProgram
+namespace ConsoleApplication
 {
-    class Addition
+    class paraconstrctor
     {
         int var1, var2, var3;
-        public Addition()   //Default constructor
+        public paraconstrctor(int x, int y)// decalaring Paremetrized Constructor with (ing x,int y) parameter
         {
-            Console.WriteLine("This program Perform Addition please Enter Any two number");
-            Console.WriteLine("Enter The First Number");
-            var1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter The Second Number");
-            var2 = int.Parse(Console.ReadLine());
-            var3 = var2 + var1;
-            Console.WriteLine("Addition of two Number is: " + var3);
-        }
+            Console.WriteLine("This Program Perform Multiplication between two number");
+            var1 = x;
+            var2 = y;
+            var3 = var2 * var1;
+            Console.WriteLine("Multiplaction of two Number is: " + var3);
 
+        }
         static void Main(string[] args)
         {
-            Addition a = new Addition(); //an object is created , constructor is called
+            paraconstrctor p = new paraconstrctor(10, 20);
             Console.ReadLine();
         }
     }
